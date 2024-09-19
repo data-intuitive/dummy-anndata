e15fcf0f4b76b2e710a869af2bf563f93f646a67
Louise Deconinck <louise.deconinck@gmail.com>
2024-09-19 21:02:26 +0200

    Add generator scripts

--- src/dummy_anndata/__init__.py
+++ src/dummy_anndata/__init__.py
@@ -1,7 +1,9 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
-
-__all__ = ["pl", "pp", "tl"]
+from .generate_dataframe import generate_dataframe
+from .generate_dataset import generate_dataset
+from .generate_dict import generate_scalar, generate_type, generate_dict
+from .generate_matrix import generate_matrix
+from .generate_vector import generate_vector
 
 __version__ = version("dummy-anndata")
