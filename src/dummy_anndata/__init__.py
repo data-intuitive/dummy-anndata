a7f7bb8d12b2d1f2fe6a76ac22a4bcef4e8f47dd
Louise Deconinck <louise.deconinck@gmail.com>
2024-09-19 21:05:11 +0200

    No imports needed, no submodules

--- src/dummy_anndata/__init__.py
+++ src/dummy_anndata/__init__.py
@@ -1,9 +1,3 @@
 from importlib.metadata import version
 
-from .generate_dataframe import generate_dataframe
-from .generate_dataset import generate_dataset
-from .generate_dict import generate_scalar, generate_type, generate_dict
-from .generate_matrix import generate_matrix
-from .generate_vector import generate_vector
-
 __version__ = version("dummy-anndata")
